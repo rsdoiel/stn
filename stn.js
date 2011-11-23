3c31038deee0577f0ddf6c625d76921ae00fe57d
R. S. Doiel <rsdoiel@gmail.com>
2011-11-23 00:10:50 +0000

    Started 0.0.2 prototype.

--- stn.js
+++ stn.js
@@ -8,7 +8,7 @@
 // Released under New the BSD License.
 // See: http://opensource.org/licenses/bsd-license.php
 //
-// revision: 0.0.1-alpha
+// revision: 0.0.2-prototype
 //
 
 var util = require('util');
